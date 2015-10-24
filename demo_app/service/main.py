ad1ebd5889afe35cc093adb60aa086caa7170d3f
Likayo Lee <likayo25@gmail.com>
2015-10-24 01:42:20 -0700

    Added a service_context variable for applications

--- demo_app/service/main.py
+++ demo_app/service/main.py
@@ -16,6 +16,7 @@ if __name__ == "__main__":
     try:
         app_file = os.path.join(get_files_dir(), "res", arg + ".mi2app")
         print "Running app: " + app_file
-        execfile(app_file)
+        namespace = {"service_context": ServiceContext}
+        execfile(app_file, namespace)
     except:
         print str(traceback.format_exc())
