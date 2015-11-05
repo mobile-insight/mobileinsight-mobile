4766796034bc9bfd262df2b78271302ba99c952b
Likayo Lee <likayo25@gmail.com>
2015-11-05 00:48:41 -0800

    Added realtime app (for backup)
    Former-commit-id: e4c103d1916ac9bfb3741e3cc15ee8f9a8183eaa

--- demo_app/service/mi2app_utils.py
+++ demo_app/service/mi2app_utils.py
@@ -7,9 +7,12 @@ Define utility variables and functions for apps.
 __all__ = [ "get_service_context",
             "get_cache_dir",
             "get_files_dir",
+            "run_shell_cmd",
             ]
 
 from jnius import autoclass
+# FIXME(likayo): subprocess module in Python 2.7 is not thread-safe. Use subprocess32 instead.
+import subprocess
 
 service_context = autoclass('org.renpy.android.PythonService').mService
 
@@ -21,3 +24,9 @@ def get_cache_dir():
 
 def get_files_dir():
     return str(service_context.getFilesDir().getAbsolutePath())
+
+def run_shell_cmd(cmd, wait=False):
+    ANDROID_SHELL = "/system/bin/sh"
+    p = subprocess.Popen(cmd, executable=ANDROID_SHELL, shell=True)
+    if wait:
+        p.wait()
