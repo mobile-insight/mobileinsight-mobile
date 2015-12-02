c49191a3cbfaed0d2046ff7f25e78923ffe9d022
Likayo Lee <likayo25@gmail.com>
2015-12-01 23:27:37 -0800

    Added support for phone model number
    Former-commit-id: b01e96ee17e7b95143f28e0c434347e2ac4ecb28

--- demo_app/service/main.py
+++ demo_app/service/main.py
@@ -23,6 +23,7 @@ if __name__ == "__main__":
         APP_DIR = os.path.join(mi2app_utils.get_files_dir(), "app")
         sys.path.append(os.path.join(APP_DIR, arg)) # add this dir to module search path
         app_file = os.path.join(APP_DIR, arg, "main.mi2app")
+        print "Phone model: " + mi2app_utils.get_phone_model()
         print "Running app: " + app_file
         namespace = {"service_context": mi2app_utils.get_service_context()}
         execfile(app_file, namespace)
