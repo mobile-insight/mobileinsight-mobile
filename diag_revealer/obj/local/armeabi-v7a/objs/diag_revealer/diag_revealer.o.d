5a04479fe87b22a3a3c46e9f2c40863195f63bbb
Yuanjie Li <yuanjie.li@cs.ucla.edu>
2016-02-16 09:54:10 -0800

    update diag_revealer
    Former-commit-id: 199ee97a601b254d76595ee5d68e75cf192fa5f6

---
+++ diag_revealer/obj/local/armeabi-v7a/objs/diag_revealer/diag_revealer.o.d
@@ -0,0 +1,164 @@
+/Users/yuanjieli/Documents/wing/cellular-analytics/code/MobileInsight2/diag_revealer/obj/local/armeabi-v7a/objs/diag_revealer/diag_revealer.o: \
+ /Users/yuanjieli/Documents/wing/cellular-analytics/code/MobileInsight2/diag_revealer/jni/diag_revealer.c \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/assert.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/endian.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/sys/endian.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/fcntl.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/sys/types.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/stdint.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/asm/types.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/linux/types.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/linux/fcntl.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/asm/fcntl.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/asm-generic/fcntl.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/unistd.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/sys/select.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/sys/time.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/linux/time.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/signal.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/limits.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/sys/limits.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/linux/limits.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/machine/internal_types.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/machine/limits.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/sys/syslimits.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/string.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/malloc.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/asm/signal.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/asm/siginfo.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/sys/sysconf.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/linux/capability.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/pathconf.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/stdio.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/stdlib.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/alloca.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/strings.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/memory.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/sys/ioctl.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/linux/ioctl.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/asm/ioctl.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/asm-generic/ioctl.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/asm/ioctls.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/asm/termbits.h \
+ /Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/sys/ioctl_compat.h
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/assert.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/endian.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/sys/endian.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/fcntl.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/linux/fcntl.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/asm/fcntl.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/asm-generic/fcntl.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/unistd.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/sys/select.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/sys/time.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/linux/time.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/signal.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/limits.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/sys/limits.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/linux/limits.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/machine/internal_types.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/machine/limits.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/sys/syslimits.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/string.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/asm/signal.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/asm-generic/signal.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/asm/siginfo.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/sys/sysconf.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/linux/capability.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/pathconf.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/stdio.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/stdlib.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/alloca.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/strings.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/memory.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/sys/ioctl.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/linux/ioctl.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/asm/ioctl.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/asm-generic/ioctl.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/asm/ioctls.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/asm/termbits.h:
+
+/Users/yuanjieli/Documents/android-ndk/platforms/android-3/arch-arm/usr/include/sys/ioctl_compat.h:
