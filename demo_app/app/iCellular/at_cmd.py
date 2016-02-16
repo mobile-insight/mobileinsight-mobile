3539708d030ec601b460668e07195d926ae06aaf
Yuanjie Li <yuanjie.li@cs.ucla.edu>
2016-02-15 16:58:53 -0800

    iCellular adaptive monitor
    Former-commit-id: 73c645182e4c63d522b4974a4493dbccb3463065

---
+++ demo_app/app/iCellular/at_cmd.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python
+# Filename: at_cmd.py
+
+"""
+
+An interface for AT commands
+
+Author: Yuanjie Li
+
+"""
+
+import sys
+import subprocess
+import codecs
+
+ANDROID_SHELL = "/system/bin/sh"
+at_log_file = "/sdcard/at_tmp.txt"
+
+class AtCmd(object):
+    
+    def __init__(self, at_device):
+
+        '''
+        Initialize the at command interface. Disable the echo mode
+
+        :param at_device: the serial port name for AT command
+        :types at_device: string
+        '''
+
+        # self._run_shell_cmd("su -c chown root "+at_device,True)
+        # self._run_shell_cmd("su -c chgrp sdcard_rw "+at_device,True)
+        self._run_shell_cmd("su -c chmod 777 " + at_device, True)
+
+        # self.phy_ser = open(at_device,"rw")
+        self.at_device = at_device
+
+        at_res_cmd = "su -c cat " + at_device + ">" + at_log_file
+        self.at_proc = subprocess.Popen(at_res_cmd, executable = ANDROID_SHELL, shell = True)
+
+        self.cmd_count = 0
+
+        #disable echo mode
+        self.run_cmd("ATE0")
+
+    def _run_shell_cmd(self, cmd, wait = False):
+        p = subprocess.Popen(cmd, executable = ANDROID_SHELL, shell = True)
+        if wait:
+            p.wait()
+            return p.returncode
+        else:
+            return None
+
+
+    def run_cmd(self, cmd, wait = False):
+        '''
+        Send an AT command, return responses
+
+        :param cmd: the command to be sent
+        :type cmd: string
+        :param wait: whether to wait for the responses of AT command
+        :type wait: boolean
+        :returns: the return value of AT command if wait==True, otherwise empty string
+        '''
+        full_cmd = 'su -c \"echo -e \'' + cmd + '\\r\\n\' > ' + self.at_device + "\""
+        p = subprocess.Popen(full_cmd, executable = ANDROID_SHELL, shell = True)
+        p.wait()
+
+        if not wait:
+            return ""
+        
+        while True:
+            res = ""
+            count = 0
+            with codecs.open(at_log_file, encoding = 'utf8') as fp:
+                while True:
+                    s = fp.readline()
+                    if not s:
+                    	break
+                    res += s
+                    if len(res) > 2 and res[-2] == "\r" and res[-1] == "\n":
+                        if count == self.cmd_count:
+                            break
+                        else:
+                        	count = count + 1
+                        	res = ""
+            if res:
+            	self.cmd_count = self.cmd_count + 1
+                return res
+
+if __name__=="__main__":
+    # at_cmd = AtCmd("/dev/smd11")
+    at_cmd = AtCmd(at_device)
+    print at_cmd.run_cmd("ATD3106148922")
--- demo_app/app/iCellular/icellular_monitor.py
+++ demo_app/app/iCellular/icellular_monitor.py
@@ -1,1 +1,142 @@
 #!/usr/bin/python
+# Filename: icellular_monitor.py
+"""
+iCellular adaptive monitor
+
+It achieves min-search and non-disruptive search.
+To do so, it initiates the manual network search, 
+trackes the current sacnning process (from MobileInsight), 
+and stops the search when necessary.
+
+Author: Yuanjie Li
+"""
+
+from at_cmd import * 
+
+from mobile_insight.analyzer import Analyzer
+
+
+class iCellularMonitor(Analyzer):
+
+	'''
+	Monitoring states
+	'''
+	NULL = 0;	#monitoring not started
+	STARTED = 1;	#manual network search is initiated   
+
+    
+    def __init__(self, monitor_list, at_serial_port):
+        '''
+        Initialize the iCellular monitor
+
+        :param monitor_list: a list of carriers to be monitored (in MNC/MCC form)
+        :type monitor_list: list
+        :at_serial_port: in-phone device that connects to AT command at_serial_port
+        :type at_serial_port: string
+        '''
+
+        #MobileInsight Analyzer init
+        Analyzer.__init__(self)
+        self.add_source_callback(self.__active_monitor)
+        #TODO: add dependency of Rrc/Nas analyzers (for profiling purpose)
+
+        self.__at_cmd = AtCmd(at_serial_port)  # AT command port
+        if monitor_list:
+            self.__monitor_list = monitor_list
+        else:
+        	self.__monitor_list = []
+        self.__monitor_state = self.NULL
+
+        #observed carrier lists: carrier -> [RSRP/RSCP, radio/QoS profile (from Rrc/Nas analyzer)]
+        #Passed to the decisions strategy
+        self.__observed_list = {}	
+
+        #Start the active monitor
+        self.run_monitor(self.__monitor_list) 	
+
+
+    def set_source(self,source):
+        """
+        Set the trace source. Enable the LTE RRC messages.
+
+        :param source: the trace source.
+        :type source: trace collector
+        """
+        Analyzer.set_source(self,source)
+        #enable LTE RRC log
+        source.enable_log("LTE_RRC_OTA_Packet")
+        source.enable_log("LTE_RRC_Serv_Cell_Info_Log_Packet")
+
+
+
+    def run_monitor(self, monitor_list):
+        '''
+        Start the active monitor (called by external source). 
+        NOTE: this function is **non-blocking**, i.e., it exits immediately after
+        manual network search is initiated.
+
+        :param monitor_list: a list of carriers to be monitored (in MNC/MCC form)
+        :type monitor_list: list
+        '''
+       
+
+        '''
+        Stop ongoing AT commands (if any)
+	    
+	    There is NO available AT commands to do it. 
+	    As a work-around, if COPS is running, making a call will terminate the search
+	    To make a phone call, DON'T USE ATD command. It does not work
+    	'''
+
+
+        #Reset observed carrier networks
+        self.__observed_list = []
+
+
+        #Configure the preferred carrier list
+
+        #Start the AT command and monitor (non-blocking mode)
+        at_res = self.__at_cmd.run_cmd("AT+COPS=?", False)
+        self.__monitor_state = self.STARTED
+
+
+    def __active_monitor(self, msg):
+
+    	'''
+    	Callback to implement min monitor.
+    	It monitors the RRC SIB in manual network search, 
+    	and stops the icellular monitor if search finishes
+    	'''
+
+
+
+    	if self.__monitor_state == self.NULL:
+    	    #No active monitor initiated
+    	    return
+    	else:
+    	    '''
+    	    Monitor incoming SIB and RSRP, detect the currently available carrier network.
+    	    Run the decision-fault prevention function. 
+    	    If the new carrier network available, raise events to downstream analyzer
+    	    (i.e., decision strategy)
+    	    '''
+
+            #TODO: support 0x1FEB  Extended Debug Message (for RSRP/RSRQ)
+            #TODO: build lists
+    	    self.send(self.__observed_list) #Currently observed carriers
+
+
+
+
+
+
+    
+
+    
+
+
+
+
+
+
+
