feaabdc1490f7048ec11f9e9b43729a4b528d7e0
Kainan Wang <knwang@cs.ucla.edu>
2016-02-27 11:10:41 -0800

    move changes of strategys to a new temp file and clean the base file
    Former-commit-id: 85dfe5e1dadff3bd164c3088ce3b6e5858b70b2c

--- demo_app/app/iCellular/decision_strategy/icellular_strategy_base.py
+++ demo_app/app/iCellular/decision_strategy/icellular_strategy_base.py
@@ -21,17 +21,3 @@ class IcellularStrategyBase(object):
         :param carrier_network_list: a dict of available carriers
         :returns: the target carrier network, or None if no switch is needed
         """
-        # best_lantency = 1e10
-        # best_carrier = None
-        # for carrier, data in carrier_network_list.items():
-        #     result = predict(self.fit, [y for x, y in data.items()])
-        #     if result < best_lantency:
-        #         best_lantency = result
-        #         best_carrier = carrier
-        # return best_carrier
-
-        return None
-
-if __name__ == '__main__':
-    test = {'att': {'1': 2,'2':-100.8,'3':0,'4': 90},'tmobile':{'1':2, '2':-109.6,'3':0, '4': 99.5 }}
-    print IcellularStrategyBase().selection(test)
---
+++ demo_app/app/iCellular/decision_strategy/icellular_strategy_temp.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# Filename: icellular_strategy_base.py
+"""
+Base class of decisions strategies.
+This defines the common interface for selection decision.
+
+Author: Yuanjie Li
+"""
+from cart_interface import getTree, predict
+from icellular_strategy_base import IcellularStrategyBase
+
+
+class IcellularStrategyTemp(IcellularStrategyBase):
+
+    def __init__(self):
+	IcellularStrategyBase.__init__(self)
+        self.fit = getTree()
+
+    def selection(self, carrier_network_list):
+        """
+        Select the target carrier network given the available candidate dict
+
+        :param carrier_network_list: a dict of available carriers
+        :returns: the target carrier network, or None if no switch is needed
+        """
+        best_lantency = 1e10
+        best_carrier = None
+        for carrier, data in carrier_network_list.items():
+            result = predict(self.fit, [y for x, y in data.items()])
+            if result < best_lantency:
+                best_lantency = result
+                best_carrier = carrier
+        return best_carrier
+
+if __name__ == '__main__':
+    test = {'att': {'1': 2,'2':-100.8,'3':0,'4': 90},'tmobile':{'1':2, '2':-109.6,'3':0, '4': 99.5 }}
+    print IcellularStrategyTemp().selection(test)
