59a192e9ce19a9f7ec826a68976e3f79b628d2ab
Yuanjie Li <yuanjie.li@cs.ucla.edu>
2016-03-29 07:52:26 -0700

    latest code
    Former-commit-id: 34fc505af4aff8a81717e843c7810d9e211c04bd

--- demo_app/service/main.py
+++ demo_app/service/main.py
@@ -16,26 +16,13 @@ if __name__ == "__main__":
     # get the argument passed
     arg = os.getenv("PYTHON_SERVICE_ARGUMENT")
 
-    try:
-        t = threading.Thread(target=alive_worker, args=(30.0,))
-        t.start()
+    t = threading.Thread(target=alive_worker, args=(30.0,))
+    t.start()
 
-        APP_DIR = os.path.join(mi2app_utils.get_files_dir(), "app")
-        sys.path.append(os.path.join(APP_DIR, arg)) # add this dir to module search path
-        app_file = os.path.join(APP_DIR, arg, "main.mi2app")
-        print "Phone model: " + mi2app_utils.get_phone_model()
-        print "Running app: " + app_file
-        namespace = {"service_context": mi2app_utils.get_service_context()}
-        execfile(app_file, namespace)
-    except:
-        try:
-            print str(traceback.format_exc())
-            APP_DIR = "/sdcard/mobile_insight_app/"
-            sys.path.append(os.path.join(APP_DIR, arg)) # add this dir to module search path
-            app_file = os.path.join(APP_DIR, arg, "main.mi2app")
-            print "Phone model: " + mi2app_utils.get_phone_model()
-            print "Running app: " + app_file
-            namespace = {"service_context": mi2app_utils.get_service_context()}
-            execfile(app_file, namespace)
-        except:
-            print str(traceback.format_exc())
+    APP_DIR = os.path.join(mi2app_utils.get_files_dir(), "app")
+    sys.path.append(os.path.join(APP_DIR, arg)) # add this dir to module search path
+    app_file = os.path.join(APP_DIR, arg, "main.mi2app")
+    print "Phone model: " + mi2app_utils.get_phone_model()
+    print "Running app: " + app_file
+    namespace = {"service_context": mi2app_utils.get_service_context()}
+    execfile(app_file, namespace)
