17632bbfa15e38f718c847753cafc3ae814eeb91
Zengwen Yuan <zyuan@cs.ucla.edu>
2016-03-28 21:12:18 -0700

    update deploy script
    Former-commit-id: 6e9e586496b294d2565c9838253d7e84ae842809

--- deploy.py
+++ deploy.py
@@ -1,43 +1,51 @@
 #!/usr/bin/python
 
 import os, sys, commands
+import yaml
 
 
 def run_config():
-    commands.getstatusoutput('cp config_template.py make_config.py')
-    print 'Edit make_config.py to set up the configuration'
+    commands.getstatusoutput('cp config_template.yml config.yml') 
+    print 'Edit config.yml to set up the configuration'
 
 
-def run_apk():
-    import make_config
+def run_apk(build_release):
+    with open("config.yml", 'r') as ymlfile:
+        cfg = yaml.load(ymlfile)
 
     build_dist_cmd = 'python-for-android create' \
-            + ' --dist_name=' + dist_name \
-            + ' --bootstrap=' + bootstrap \
-            + ' --requirements=' + requirements
+            + ' --dist_name=' + cfg['dist_name'] \
+            + ' --bootstrap=' + cfg['bootstrap'] \
+            + ' --requirements=' + cfg['requirements']
 
     build_cmd = 'python-for-android apk' \
-            + ' --debug' \
             + ' --compile-pyo' \
             + ' --copy-libs' \
-            + ' --name' + app_name \
-            + ' --version' + app_version \
-            + ' --private' + app_path \
-            + ' --package' + pkg_name \
+            + ' --name=' + cfg['app_name'] \
+            + ' --version=' + str(cfg['app_version']) \
+            + ' --private=' + cfg['app_path'] \
+            + ' --package=' + cfg['pkg_name'] \
             + ' --permission INTERNET' \
             + ' --permission WRITE_EXTERNAL_STORAGE' \
-            + ' --icon' + icon_path \
-            + ' --presplash' + presplash_path \
-            + ' --orientation' + orientation \
-            + ' --sdk' + sdk_version \
-            + ' --minsdk' + minsdk \
-            + ' --android_api' + api_level \
-            + ' --sdk_dir' + sdk_path \
-            + ' --ndk_dir' + ndk_path \
-            + ' --ndk_version' + ndk_version \
-            + ' --arch' + arch \
-            + ' --dist_name' + dist_name \
-            + ' --whitelist' + whitelist
+            + ' --icon=' + cfg['icon_path'] \
+            + ' --presplash=' + cfg['presplash_path'] \
+            + ' --orientation=' + cfg['orientation'] \
+            + ' --sdk=' + str(cfg['sdk_version']) \
+            + ' --minsdk=' + str(cfg['minsdk']) \
+            + ' --android_api=' + str(cfg['api_level']) \
+            + ' --sdk_dir=' + cfg['sdk_path'] \
+            + ' --ndk_dir=' + cfg['ndk_path'] \
+            + ' --ndk_version=' + str(cfg['ndk_version']) \
+            + ' --arch=' + cfg['arch'] \
+            + ' --dist_name=' + cfg['dist_name'] \
+            + ' --whitelist=' + cfg['whitelist']
+
+    if build_release is True:
+        build_cmd = build_cmd + "--release" \
+                + ' --keystore=' + cfg['keystore']\
+                + ' --signkey=' + cfg['signkey']\
+                + ' --keystorepw=' + cfg['keystorepw']\
+                + ' --signkeypw=' + cfg['signkeypw']
 
     print build_dist_cmd
     os.system(build_dist_cmd)
@@ -47,12 +55,22 @@ def run_apk():
 
 if __name__ == '__main__':
     arg = sys.argv[1]
+    try:
+        debug = sys.argv[2]
+    except:
+        debug = ""
+
     if arg == 'config':
-        run_config();
+        run_config()
     elif arg == 'apk':
         # TODO:
         # support debug version and release version
-        run_apk();
+        if debug == "debug" or debug == "":
+            run_apk(False)
+        elif debug == "release":
+            run_apk(True)
+        else:
+            print "Usage: python deploy.py apk [debug|release]"
     elif arg == 'clean':
         # TODO
         # finish clean up script
