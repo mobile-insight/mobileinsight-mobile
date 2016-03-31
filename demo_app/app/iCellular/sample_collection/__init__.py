d57a3f3792f7d6c431d0e8df1bf7d426de63a2c1
Zengwen Yuan <zyuan@cs.ucla.edu>
2016-03-30 20:33:50 -0700

    move iCellular and Realtime to internal apps
    Former-commit-id: 368b116768049084b6271c1a31250d28cbb43131

--- demo_app/app/iCellular/sample_collection/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__all__=["SampleCollectionBase","OpensignalLatency","RadioLinkCapacity"]
-
-from sample_collection_base import SampleCollectionBase
-from opensignal_latency import OpensignalLatency
-from radio_link_capacity import RadioLinkCapacity
