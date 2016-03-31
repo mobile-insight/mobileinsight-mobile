d13de8a7bdb2054aa5c3a2c9e7d6b6db63a28985
Yuanjie Li <yuanjie.li@cs.ucla.edu>
2016-03-30 21:52:48 -0700

    commit before checkout
    Former-commit-id: b59ff3f632f4d7ed3ba217675a1368c7f6e2b0e2

--- demo_app/app/iCellular/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__all__=["IcellularAnalyzer","IcellularDecision","IcellularExec","IcellularMonitor","IcellularSampleCollection"]
-
-from icellular_analyzer import IcellularAnalyzer
-from icellular_decision import IcellularDecision
-from icellular_exec import IcellularExec
-from icellular_monitor import IcellularMonitor
-from icellular_sample_collection import IcellularSampleCollection
--- demo_app/app/iCellular/decision_strategy/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-__all__=["TestStrategy","IcellularStrategyBase","IcellularPredictionStrategy"]
-
-from icellular_strategy_base import IcellularStrategyBase
-from test_strategy import TestStrategy
-from icellular_prediction_strategy import IcellularPredictionStrategy
