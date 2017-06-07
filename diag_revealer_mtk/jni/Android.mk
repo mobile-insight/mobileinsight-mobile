d3b455858ef1e3dc489d451281a3581f7eed12df
Yuanjie Li <yuanjie.li@cs.ucla.edu>
2017-06-06 17:54:25 -0700

    update diag_revealer

--- diag_revealer_mtk/jni/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE    := diag_revealer_mtk
-LOCAL_SRC_FILES := diag_revealer_mtk.c
-LOCAL_LDLIBS    := -L$(SYSROOT)/usr/lib -llog
-
-include $(BUILD_EXECUTABLE)
