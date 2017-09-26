8ae2bae86b2c0d297354540080d6f76a539964e9
Zengwen Yuan <zyuan@cs.ucla.edu>
2017-09-26 12:50:25 -0700

    fix: new lib path under new p4a

--- app/main_utils.py
+++ app/main_utils.py
@@ -262,7 +262,7 @@ def get_cache_dir():
 
 
 def get_files_dir():
-    return str(current_activity.getFilesDir().getAbsolutePath())
+    return str(current_activity.getFilesDir().getAbsolutePath() + '/app')
 
 def get_phone_manufacturer():
     return androidOsBuild.MANUFACTURER
