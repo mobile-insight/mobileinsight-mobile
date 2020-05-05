d0bb6006e6dc4aac4536e3d266b2de70ac0c88ac
Boyan Ding <boyan.j.ding@gmail.com>
2020-05-04 21:44:10 -0700

    get_chipset_type: compare with bytes instead of str
    Python 3 not returns 'bytes' for piped command

--- app/main_utils.py
+++ app/main_utils.py
@@ -106,9 +106,9 @@ def get_chipset_type():
     """
     cmd = "getprop ro.board.platform;"
     res = run_shell_cmd(cmd)
-    if res.startswith("mt"):
+    if res.startswith(b"mt"):
         return ChipsetType.MTK
-    elif res.startswith("msm") or res.startswith("mdm") or res.startswith("sdm"):
+    elif res.startswith(b"msm") or res.startswith(b"mdm") or res.startswith(b"sdm"):
         return ChipsetType.QUALCOMM
     else:
         return None
